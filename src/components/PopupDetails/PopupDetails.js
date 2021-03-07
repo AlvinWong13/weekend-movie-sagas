b2ab66b225dbf70e1380a48351f5f754f64e467e
Alvin Wong <wong.alvin13@gmail.com>
2021-03-06 22:00:02 -0600

    popups

---
+++ src/components/PopupDetails/PopupDetails.js
@@ -0,0 +1,39 @@
+import React from 'react'
+import { Dialog, DialogTitle, DialogContent, makeStyles, Typography } from '@material-ui/core';
+import Controls from '../Controls/Controls';
+import CloseIcon from '@material-ui/icons/Close';
+
+const useStyles = makeStyles(theme => ({
+    dialogWrapper : {
+      padding: theme.spacing(2),
+      position:'absolute',
+      top: theme.spacing(5)
+    },
+    dialogTitle: {
+      paddingRight: '0px'
+    }
+}))
+
+function PopupDetails(props) {
+
+  const { children, openDetails, setOpenDetails } = props;
+  const classes = useStyles();
+
+  return (
+    <Dialog open={openDetails} classes={{ paper :classes.dialogWrapper }}>
+        <DialogTitle>
+              <Controls.ActionButton
+                color="secondary"
+                variant="outlined"
+                onClick={() => {setOpenDetails(false)}}>
+                <CloseIcon />
+              </Controls.ActionButton>
+        </DialogTitle>
+        <DialogContent dividers>
+            {children}
+        </DialogContent>
+    </Dialog>
+  )
+}
+
+export default PopupDetails;
---
+++ src/components/PopupForm/PopupForm.js
@@ -0,0 +1,44 @@
+import React from 'react'
+import { Dialog, DialogTitle, DialogContent, makeStyles, Typography } from '@material-ui/core';
+import Controls from '../Controls/Controls';
+import CloseIcon from '@material-ui/icons/Close';
+
+const useStyles = makeStyles(theme => ({
+    dialogWrapper : {
+      padding: theme.spacing(2),
+      position:'absolute',
+      top: theme.spacing(5)
+    },
+    dialogTitle: {
+      paddingRight: '0px'
+    }
+}))
+
+function PopupForm(props) {
+
+  const { title, children, openForm, setOpenForm } = props;
+  const classes = useStyles();
+
+  return (
+    <Dialog open={openForm} maxWidth="md" classes={{ paper :classes.dialogWrapper }}>
+        <DialogTitle>
+            <div style={{display: 'flex'}}>
+              <Typography variant="h5" component="div" style={{flexGrow:1}}>
+                {title}
+              </Typography>
+              <Controls.ActionButton
+                color="secondary"
+                variant="outlined"
+                onClick={() => {setOpenForm(false)}}>
+                <CloseIcon />
+              </Controls.ActionButton>
+            </div>
+        </DialogTitle>
+        <DialogContent dividers>
+            {children}
+        </DialogContent>
+    </Dialog>
+  )
+}
+
+export default PopupForm;
